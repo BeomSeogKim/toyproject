9d52c766e935d9139b681881696b1cc7d592e783
BeomSeogKim <kbs4520@daum.net>
2023-03-13 02:13:48 +0900

    add HealCheck

---
+++ src/main/java/tommy/toyproject/apicontroller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package tommy.toyproject.apicontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "ok";
+    }
+}
